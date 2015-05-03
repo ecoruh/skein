1ee74aa20d0b3a870ab5621581eca26b0dffb61f
ecoruh <ecoruh1@gmail.com>
2015-05-03 17:56:45 +1000

    win and darwin selection

---
+++ index.js
@@ -0,0 +1,11 @@
+var skein = null;
+ 
+// Load the precompiled binary for windows.
+if(process.platform == "win32") {
+   skein = require('./bin/win/skein');
+} else if(process.platform == "darwin") {
+   skein = require('./bin/osx/skein');
+}
+ 
+module.exports = skein;
+	
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 var assert = require('assert');
-var skein = require('./build/Release/skein');
+var skein = require('./index.js');
 assert.equal('skein', skein.name());
-console.log('skein.name() =', skein.name());
+console.log('skein.name() =', skein.name()); 
