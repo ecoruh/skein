0d7228d20fb3922f0e8c748f5512f577ac3b4465
Ergun Coruh <ecoruh1@gmail.com>
2015-05-04 19:25:16 +1000

    Removed build and deployed new version

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 var assert = require('assert');
-var skein = require('./index.js');
+var skein = require('skein');
 assert.equal('skein', skein.name());
 console.log('skein.name() =', skein.name()); 
